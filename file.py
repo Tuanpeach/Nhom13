7bcf2b1b9ef61708eb351a3fdff66331d58ff649
Tuanpeach <145430691+Tuanpeach@users.noreply.github.com>
2023-09-19 15:01:51 +0700

    Update file.py add nhap so

--- file.py
+++ file.py
@@ -3,9 +3,12 @@
 # Yêu cầu hoàn chỉnh lại đoạn code
 #để có 1 app giải hệ phương trình có n phương trình n ẩn
 import numpy as np
-A = np.array([(1,2),(3,4)])
-B = np.array([5,6])
-A1  = np.linalg.inv(A) # tạo ma trận nghich đảo
+a=int(input("phuong trinh co so hang: "))
+b=int(input("nhap so cot cua ma tran"))
+arr=np.array(a,b)
+for i in range(a):
+    for i in range(b):
+        arr[i][j]=int(input(f"phan tu thu {j} hang {i} la: ".fomat(j,i)))
 print(A)
 print(B)
 print(A1)
